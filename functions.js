5b7ef37ad84f077b902330978733220cff463a18
Alfredo Gonzalez <agonzalez@mvs.com>
2022-07-19 10:28:39 -0500

    switch cases

--- functions.js
+++ functions.js
@@ -1,53 +1,63 @@
 $(document).ready(()=>{
     $('#start').click(()=>{
         $.getJSON('data.json', (el)=>{
-            eachElementsAddress(el.address);
-            eachElementsDrivers(el.drivers);
+            eachElements(el.address, el.drivers);
+            // eachElementsAddress(el.address);
+            // eachElementsDrivers(el.drivers);
         });
     });
 });
 
-/**
- * Functions for address
- * @param {*} arr 
- */
-function eachElementsAddress(arr){
-    $.each(arr, (i, item)=>{
-        console.log(i, item.street);
-        let long = getLongStreet(item);
-        let par = isPar(long);
-        let vocales = getVocales(item);
-        console.log('long: '+long+', par: '+par+', vocales: '+vocales);
-
+function eachElements(addressArr, driversArr){
+    let ss;
+    let assignRoutes = [];
+    $.each(driversArr, (i, driverElement)=>{
+        console.log(i, driverElement);
         
+        let longitudNombreConductor = getLongitud(driverElement.driver);
+        let conductorVocales = getVocales(driverElement);
+        let consuctorConsonantes = getConsonantes(driverElement);
+
+        $.each(addressArr, (j, addressElement) => {
+            let longitudNombreCalle = getLongitud(addressElement.street);
+            //console.log(j+' long street: '+longitudNombreCalle);      
+            let addressPar = isPar(longitudNombreCalle);
+            //console.log('longitud calle es par: '+addressPar);
+            
+            if(longitudNombreCalle == longitudNombreConductor) addressPar = 2;
+            
+            switch(addressPar){
+                case 2:
+                    ss = conductorVocales * 2.25;
+                case 1:
+                    ss = conductorVocales * 1.5;
+                    break;
+                case 0:
+                    ss = consuctorConsonantes * 1;
+                    break;
+            }
+            console.log('ss, par :' + ss);
+
+        });
 
     });
 }
 
-function getLongStreet(item){
-    return item.street.length;
+function getLongitud(element){
+    return element.length;
 }
 
 function isPar(long){
-    //let l = item.street.length;
     if(long%2 == 0) return 1;
     return 0;
 }
 
-function getVocales(item){
-    let v = item.street.match(/[aeiou]/gi);
+function getVocales(element){
+    let v = element.driver.match(/[aeiou]/gi);
     return v === null ? 0 : v.length;
 }
 
-/**
- * functions for drivers
- */
- function eachElementsDrivers(arr){
-    $.each(arr, (i, item)=>{
-        console.log(i, item.driver);
-        // let long = getLongStreet(item);
-        // let par = isPar(item);
-        // let vocales = getVocales(item);
-        // console.log('long: '+long+', par: '+par+', vocales: '+vocales);
-    });
+function getConsonantes(element){
+    let v = element.driver.match(/[bcdfghjklmnñpqrstvxzwy]/gi);
+    return v === null ? 0 : v.length;
 }
